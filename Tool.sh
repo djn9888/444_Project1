443938210887e021345c5e58efbd0857a4a04e62
unknown <david_niggli@urmc.rochester.edu>
2022-03-18 16:36:23 -0400

    UPDATE: moved APM startup to function in Tool.sh

--- Tool.sh
+++ Tool.sh
@@ -1,38 +1,48 @@
 # usr/bin/bash
 
 
-function finish {
+finish(){
 
-./killAllApm.sh
+  ./killAllApm.sh
 
 }
 trap finish EXIT
 
+startAPMs(){
+   n=1
+   while [ $n -le 6 ]
+   do
+     nohup ./APM$n 142.250.80.46 &>/dev/null &
+     n=$(($n + 1))
+   done
+   echo 'All APMs running'
+}
+
+# check for arguments and use first one in 
+# while loop if present
 a1=20
 if [ $# -gt 0 ]
   then
   a1=$1
 fi
 
-n=1
-while [ $n -le 6 ]
-do
- nohup ./APM$n 142.250.80.46 &>/dev/null &
- n=$(($n + 1))
-done
-echo 'All APMs running'
+####### START APM1-6 SECTION ########
+#start APM processes by calling function
+startAPMs
 
-
-
-#run System Monitor srpits
-#n=1
-#while [ $n -le 6 ]
-#do
-#nohup ./monitor/APM_${n}_Monitor.sh &>/dev/null &
-# n=$(($n + 1))
-#done
+###### SYSTEM MONITOR SECTION ##########
+#run System Monitor scrpit located in /monitor folder
 nohup ./monitor/APM_Monitor.sh &>/dev/null &
 echo 'Monitor running'
+
+
+####### IFSTAT SECITON ######
+#set ifstat update interval to every second.
+#default is 60 seconds
+ifstat -d 1
+
+
+##### IOSTAT SECTION ######
 
 
 #**testing, remove for PROD***
