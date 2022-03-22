d777eb8baddfd5cda7075f4b326d3da18c174141
unknown <david_niggli@urmc.rochester.edu>
2022-03-22 15:16:07 -0400

    UPDATE: cleanup, remove old code

--- Tool.sh
+++ Tool.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-
+#kill all APM and ifstat prcocesses
 cleanup(){
 
   for pid in $(ps aux | grep -v grep | grep APM | awk '{print $2}' )
@@ -18,6 +18,7 @@ cleanup(){
 }
 trap cleanup EXIT
 
+#start all APM prcocesses in headless mode
 startAPMs(){
    n=1
    while [ $n -le 6 ]
@@ -32,11 +33,9 @@ writeSystemMetrics() {
 
    time=$1
 
-   #output="$time"
-
    # network statistics section (-10 for now to remind us to fix it)
-
    net=$(ifstat ens192 -t 1 | awk '{if(NR==4) print $3,",",$5}')
+
    # hard disk section
    # measure hard disk writes in kB/section to the primary hard drive
    # kB_wrtn/s is the fourth column
@@ -52,9 +51,11 @@ writeSystemMetrics() {
 }
 
 writeProcessMetrics(){
-
-
+  
    times=$1
+   
+   #set array for number of APM prcocesses running
+   #then loop through them with ps aux to gather process data
    n=(1 2 3 4 5 6)
       y="$times,"
       for apm in "${n[@]}"
@@ -67,13 +68,14 @@ writeProcessMetrics(){
       done
 
    times=$(($times+5))
+   #write to process monitor log
    echo "$y">>log/apm1Monit.csv
 
 }
 
 # check for arguments and use first one in 
 # while loop if present
-a1=20
+a1=900
 if [ $# -gt 0 ]
   then
   a1=$1
@@ -87,35 +89,29 @@ startAPMs
 #run System Monitor scrpit located in /monitor folder
 echo "Time,APM1%CPU,APM1%MEM,APM2%CPU,APM2%MEM,APM3%CPU,APM3%MEM,APM4%CPU,APM4%MEM,APM5%CPU,APM5%MEM,APM6%CPU,APM6%MEM,">log/apm1Monit.csv
 
-#nohup ./monitor/APM_Monitor.sh &>/dev/null &
-#echo 'Monitor running'
-
 
 ####### IFSTAT SECTION ######
 #set ifstat update interval to every second.
 #default is 60 seconds
-
 ifstat -d 1
 
 ###### SYSTEM METRICS ######
 # run system metrics script located in /monitor folder
 # contains code for both network and hard disk stats
-# nohup ./monitor/APM_system_monitor.sh &>/dev/null &
-# echo 'System metrics monitor running'
 echo "Time,RX Data Rate,TX Data Rate,Disk Writes,Disk Capacity,">./log/system_metrics.csv
 
 #**testing, remove for PROD***
 #loop for how ever many seconds specified in while 
+#default set to 15min(900 secs)
 x=1
 while [ $x -le $a1 ]
 do
 
-if [ $(($x % 5)) -eq 0 ]; then
-   writeSystemMetrics $x
-   writeProcessMetrics $x
-fi
-
-echo $x
-x=$(($x + 1))
-sleep 1
+   if [ $(($x % 5)) -eq 0 ]; then
+      writeSystemMetrics $x
+      writeProcessMetrics $x
+   fi
+   echo $x
+   x=$(($x + 1))
+   sleep 1
 done
